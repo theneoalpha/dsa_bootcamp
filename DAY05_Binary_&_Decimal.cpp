39c061ce3ad0030d37a4aceee193fc2c92da5d97
Vikash <vikashkaushik910@gmail.com>
2023-10-10 11:44:32 +0530

    Decimal to octal and octal to binary conversion

--- DAY05_Binary_&_Decimal.cpp
+++ DAY05_Binary_&_Decimal.cpp
@@ -32,7 +32,7 @@ int num = 13;
 
 */
 
-
+/*
   // Binary to Decimal Conversion
 
 cout<<"Binary to decimal Conversion"<<endl;
@@ -46,7 +46,34 @@ cout<<"Binary to decimal Conversion"<<endl;
     mul = mul * 2;
   }
   cout<<"The Decimal representation of given number is :"<< ans;
+*/
 
-
+/*
+  // Decimal to octal
   
+  cout<<"Decimal to octal coversion"<<endl;
+int num = 13;
+int ans = 0,mul = 1, rem;
+  while(num!= 0){
+    rem = num % 8;
+    num = num/8;
+    ans = rem * mul+ans;
+    mul = mul * 10;
+    
+  }
+  cout<<"The octal representation of given deimal is :" <<ans;
+*/
+
+  // Octal to Decimal
+
+  cout<<"Octal to Decimal conversion\n";
+  int num = 15;
+  int ans =0,mul = 1,rem;
+  while(num !=0){
+    rem = num % 10;
+    num = num/10;
+    ans = rem*mul + ans;
+    mul = mul * 8;
+  }
+  cout<<"The Binary converion of octal number is :"<<ans;
 }
