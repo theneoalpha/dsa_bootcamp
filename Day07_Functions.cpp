107f0716c45cc06fcb5b022735d4407015aaa407
Vikash <vikashkaushik910@gmail.com>
2023-10-18 11:25:35 +0530

    Factorial of a number using function

--- Day07_Functions.cpp
+++ Day07_Functions.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-
+/*
 bool Prime(int n) {
   if(n<2){
     cout<<"Not a Prime number";
@@ -15,18 +15,33 @@ bool Prime(int n) {
   }
   return 1;
 }
+*/
 
+int Fact(int a){
+  int value = 1;
+  for(int i = 1;i<=a;i++){
+    value = value*i;
+    
+  }
+  return value;
+}
 
 int main() {
   cout << "Day 07:  Functions \n ";
 
 
- 
+ /* 
    // Prime Number using function
   int c = Prime(8);
   cout<<c<<endl;
   int d = Prime(17);
   cout<<d<<endl;
- 
+ */
+
+  //        Factorial of a number using function
+  // 5! = 5*4*3*2*1 = 120
+  int e = Fact(5);
+  cout<<e;
+
 
 }
