3be5a2ea14e33fdb4c2b23701ff628d6ea30c94a
Vikash <vikashkaushik910@gmail.com>
2023-10-27 13:22:25 +0530

    Selection sorting of an array element in increasing order

---
+++ Day11_Array_Sorting.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+
+int main() {
+
+  int arr[5] = {5,6,2,9,7};
+  cout << "Day 11 : Arrays Sorting \n";
+    // Problem 01:  Selection Sorting in increasing order by checking  first to last element of an array
+  for(int i=0;i<5;i++){
+    int index = i;
+    for(int j=i+1;j<4;j++){
+      if(arr[j]<arr[index])
+        index = j;
+    }
+    swap(arr[index],arr[i]);
+  }
+    for(int i=0;i<5;i++){
+      cout<<arr[i]<<" ";
+    }
+}
