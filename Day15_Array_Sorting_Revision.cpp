bf0d2d5ae7e7ac97c3386c1b79c0deccc6f0b433
Vikash <vikashkaushik910@gmail.com>
2023-11-08 22:37:18 +0530

    Revision of Insertion sort

--- Day15_Array_Sorting_Revision.cpp
+++ Day15_Array_Sorting_Revision.cpp
@@ -29,7 +29,7 @@ for(int i =0;i<5;i++){
   }
 
 */
-
+/*
 // Sorting Method 02 : Bubble sort
 
   cout<<"Array sorting : Bubble sort \n";
@@ -53,9 +53,28 @@ for(int i =0;i<5;i++){
     cout<<arr[i]<<" ";
   }
 
- 
+ */
+// Sorting Method 03 : Insertion sort
+  cout << "Insertion Sort\n";
 
 
+  int arr[5] = {45,68,23,34,19};
+
+  for(int i=1;i<5;i++){
+    for(int j = i;j>0;j--){
+      if(arr[j-1]>arr[j]){
+        swap(arr[j],arr[j-1]);
+      }
+      else{
+        break;
+      }
+    }
+  }
+
+  for(int i=0;i<5;i++){
+    cout<<arr[i]<<" ";
+  }
+  
 
 
 
